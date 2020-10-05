e5d01ce4bea9361a179a7d3651dc7525e2808f48
Hubert <hubert.struminski@gmail.com>
2020-10-05 20:37:44 +0200

    add exercises 1, 2, 3 from directory 2

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,23 @@
+//Zadanie 1
+const number = 4;
+const stringValue = "Hubert Struminski";
+const numString = "Hubert" + 13;
+const boolValue = false;
+const object = null;
+
+console.log(number); // 4
+console.log(stringValue); // Hubert Struminski
+console.log(numString); // Hubert13
+console.log(boolValue); // false
+console.log(object); // null
+
+//Zadanie 2
+const num1 = 13;
+const num2 = 27;
+let sum = 0;
+sum = num1 + num2;
+console.log(sum); // 40
+
+//Zadanie 3
+let x;
+console.log(x); // undefined => caused because variable doesn't store value
