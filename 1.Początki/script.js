b66488f39535b5c05de8f5c974444c59f8c56275
Hubert <hubert.struminski@gmail.com>
2020-10-05 20:30:02 +0200

    add exercises 1, 2, 3 from directory 1

---
+++ 1.Początki/script.js
@@ -0,0 +1,12 @@
+//Zadanie 1
+console.log("Hubert"); // Hubert
+
+//Zadanie 2
+console.log(12 + 32); // 44
+console.log(52 / 16); // 3.25
+console.log(5 * 2); // 10
+
+//Zadanie 3
+console.log("Hello World"); // Hello World
+console.log(Hello World); // Syntax error - the exception caused by pass variable which doesn't exist
+console.log("Hello World); // Syntax error - caused by missing quote
